9e4b8dd35f36b519e44bdc25ba4573d2039a96df
GreteNeilinn <grete.neilinn@gmail.com>
2023-06-08 00:24:37 +0300

    github pages fix

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
-    publicPath: '/loputoo_chatgpt/'
+    publicPath: '/loputoo_projekt/'
 })
